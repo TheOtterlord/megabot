b1a6683eabc811c8cb447375577981fd999f61a3
Reuben Tier <reuben.tier@icloud.com>
2022-01-31 22:14:58 +0000

    Add subcommand example (/about)

--- src/commands/cmd.about.ts
+++ src/commands/cmd.about.ts
@@ -5,12 +5,22 @@ import App from '../app'
 export default {
 	data: new SlashCommandBuilder()
 		.setName('about')
-		.setDescription('About the bot'),
+		.setDescription('About the bot')
+		.addSubcommand(subcommand => subcommand
+			.setName('bot')
+			.setDescription('Info about the discord bot'))
+		.addSubcommand(subcommand => subcommand
+			.setName('developer')
+			.setDescription('Info about the bot developer(s)')),
 	async execute(app: App, interaction: CommandInteraction) {
 		try {
-      interaction.reply({
-        content: 'Pong!'
-      })
+			if (interaction.options.getSubcommand() === 'developer') {
+				// @todo add developer info
+				interaction.reply('The bot was developed by the following developers: Otterlord')
+			} else if (interaction.options.getSubcommand() === 'bot') {
+				// @todo add bot info
+				interaction.reply(`This is a template bot with minimal functionality. Check out the source code at https://github.com/TheOtterLord/megabot`)
+			}
 		} catch (err: any) {
       app.log.error(`${err.name}: ${err.message}\n${err.stack}`)
       interaction.reply('Something went wrong!')
